109fde85eb884a8b73f5d73fe3355d933dce4f48
forzaster <forzaster@gmail.com>
2022-09-27 22:15:49 +0900

    Add ctrl+y command

--- src/keymap.ts
+++ src/keymap.ts
@@ -1,26 +1,35 @@
 import * as vscode from 'vscode';
 
+
 export function registerCommands(context: vscode.ExtensionContext) {
     console.log('registerCommands in keymap')
 
     const emacsExt = new EmacsExt();
 
+    function registerCommand(commandid: string, command_func: (...args: any[]) => any) {
+        let command = vscode.commands.registerCommand(commandid, command_func);
+        context.subscriptions.push(command);    
+    }
+
     // ctrl+k : delete line
-    let command = vscode.commands.registerCommand('minimal-emacs.ctrl+k', () => {
+    registerCommand('minimal-emacs.ctrl+k', () => {
         emacsExt.deleteLine()
     });
-    context.subscriptions.push(command);
+    // ctrl+y : paste text buffer
+    registerCommand('minimal-emacs.ctrl+y', () => {
+        emacsExt.pasteTextBuffer()
+    });
 
     console.log('registerCommands in keymap done')
 }
 
 class EmacsExt {
     private _textBuffer: string;
-    private _pos_atTextBufferred: vscode.Position;
+    private _posAtTextBufferred: vscode.Position;
 
     constructor() {
         this._textBuffer = ''
-        this._pos_atTextBufferred = new vscode.Position(0, 0);
+        this._posAtTextBufferred = new vscode.Position(0, 0);
     }
 
     private checkState() {
@@ -31,7 +40,7 @@ class EmacsExt {
         if (!this.checkState()) {
             return;
         }
-        //console.log('delete line ' + this._pos_atTextBufferred.line + ',' + this._pos_atTextBufferred.character + ' ' + this._textBuffer)
+
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -43,14 +52,12 @@ class EmacsExt {
         }
         //console.log(line)
 
-        let selection = editor?.selection
+        let selection = editor.selection
         if (!selection) {
             return;
         }
 
         let endPos = new vscode.Position(selection.start.line, line.text.length)
-        console.log(selection.start.character)
-        console.log(line.text.length)
         if (selection.start.character == line.text.length) {
             // New line
             endPos = new vscode.Position(selection.start.line+1, 0)
@@ -61,12 +68,30 @@ class EmacsExt {
         //console.log('remove ' + text)
         editor.edit(builder => builder.delete(target))
 
-        if (selection.start == this._pos_atTextBufferred) {
+        if (selection.start == this._posAtTextBufferred) {
             this._textBuffer += text
         } else {
             this._textBuffer = text
         }
-        this._pos_atTextBufferred = selection.start
+        this._posAtTextBufferred = selection.start
         //console.log('textBuffer ' + this._textBuffer)
+    }
+
+    public pasteTextBuffer() {
+        if (!this.checkState()) {
+            return;
+        }
+
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        
+        let selection = editor.selection
+        if (!selection) {
+            return;
+        }
+
+        editor.edit(builder => builder.insert(selection.start, this._textBuffer))
     }
 }
