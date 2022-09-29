32ad8f1a0acf6c9837c9bcbb949db06f966cc85a
forzaster <forzaster@gmail.com>
2022-09-29 20:05:44 +0900

    Replace Hello World command with Show Info command to show version

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,6 @@ import {registerCommands} from './keymap';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "minimal-emacs-keymaps" is now active!');
@@ -15,10 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('minimal-emacs-keymaps.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('minimal-emacs-keymap.showInfo', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from minimal-emacs-keymaps!');
+        let packageJson = vscode.extensions.getExtension(context.extension.id)?.packageJSON
+        vscode.window.showInformationMessage('Hello World from minimal-emacs-keymaps v' + packageJson['version'] + '!');
     });
     context.subscriptions.push(disposable);
 
